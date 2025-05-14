e96603337c429b4a76c74b518cc0b6abba95cd58
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-14 22:08:14 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/2.5/exercise_4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using std::cout; // взятие объекта потока вывода cout из пространства имен
+
+int main()
+{
+	setlocale(LC_ALL, "RUS"); // функция для настройки региональных региональных
+
+	cout << "Приветствуем вас в калькуляторе квартплаты!\n";
+
+	int FullCostRepair = 4000000, NumberApartaments = 40, Entrance = 10;
+	int CostOneApartments = FullCostRepair / (Entrance * NumberApartaments);
+
+	cout << "Введите сумму, указанную в квитанции: " << FullCostRepair << '\n';
+	cout << "Сколько подъездов в вашем доме? " << Entrance << '\n';
+	cout << "Сколько квартир в каждом подъезде? " << NumberApartaments << '\n';
+	cout << "----Считаем----- \n";
+	cout << "Каждая квартира должна платить по " << CostOneApartments << " руб.\n";
+
+	return 0;
+}
