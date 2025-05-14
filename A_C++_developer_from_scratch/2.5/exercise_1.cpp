7ac548f48eeb60cea81a8496640a15e10a49ff0b
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-14 22:07:32 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/2.5/exercise_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream> 
+int main() 
+{
+	setlocale(LC_ALL, "RUS"); // Функция для настройки региональных параметров
+
+	int lap = 4;			// круг
+	int engine = 254;		// двигатель
+	int wheels = 93;		// колеса
+	int steerinWheel = 49;	// руль
+	int wind = 21;			// ветер
+	int rain = 17;			// дождь
+
+	int speed = engine + wheels + steerinWheel - wind - rain;		// скорость
+
+
+	std::cout << "===================\n";
+	std::cout << "Супер гонки. Круг " << lap << "\n";
+	std::cout << "===================\n";
+	std::cout << "Шумахер (" << speed << ")\n";
+	std::cout << "===================\n";
+	std::cout << "Водитель: Шумахер\n";
+	std::cout << "Скорость: " << speed << "\n";
+	std::cout << "-------------------\n";
+	std::cout << "Оснащение\n";
+	std::cout << "Двигатель: +" << engine << "\n";
+	std::cout << "Колеса: +" << wheels << "\n";
+	std::cout << "Руль: +" << steerinWheel << "\n";
+	std::cout << "-------------------\n";
+	std::cout << "Действия плохой погоды\n";
+	std::cout << "Ветер: -" << wind << "\n";
+	std::cout << "Дождь: -" << rain << "\n";
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/2.5/exercise_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using std::cout; // взятие объекта потока вывода cout из пространства имен
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");   // Функция для настройки региональных параметров
+
+    int discount = 30;          // скидка
+    int CostProduct = 1000;   // стоимость товара
+    int CostDelivery = 1000;    // стоимость доставки
+
+    cout << "Стоимость товара: " << CostProduct << '\n';
+    cout << "Стоимость доставки: " << CostDelivery << '\n';
+    cout << "Скидка: " << discount << '\n';
+
+    int TotalCost = CostProduct + CostDelivery;
+    int FinalCost = TotalCost - ((TotalCost / 100) * discount);
+
+    cout << "Общая стоимость товара: " << FinalCost << '\n';
+
+    return 0;
+}
