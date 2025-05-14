389f17761b23e4b4aadd73abb39179951abfcd87
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-14 22:06:28 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/1.7.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "========================" << endl
+		<< "Hello," << endl
+		<< "World" << endl
+		<< "========================" << endl;
+
+	cout << '\n';
+
+	cout << "========================" << endl
+		<< "||      Hello,        ||" << endl
+		<< "||      World         ||" << endl
+		<< "========================" << endl;
+
+	cout << '\n';
+
+	cout << "========================" << endl
+		<< "||                    ||" << endl
+		<< "||      Hello,        ||" << endl
+		<< "||      World         ||" << endl
+		<< "||                    ||" << endl
+		<< "========================" << endl;
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/2.3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int countCircle = 4;
+	int speed = 358;
+	int engineIndicator = 254;
+	int wheelsIndicator = 93;
+	int rudderIndicator = 49;
+	int WindCount = -21;
+	int RainCount = -17;
+
+
+	cout << "===================" << endl;
+	cout << "Супер гонки. Круг " << countCircle << endl;
+	cout << "===================" << endl;
+	cout << "Шумахер (" << speed << ")" << endl;
+	cout << "===================" << endl;
+	cout << "Водитель: Шумахер" << endl;
+	cout << "Скорость: " << speed << endl;
+	cout << "-------------------" << endl;
+	cout << "Оснащение" << endl;
+	cout << "Двигатель: " << engineIndicator << endl;
+	cout << "Колеса: " << wheelsIndicator << endl;
+	cout << "Руль: " << rudderIndicator << endl;
+	cout << "-------------------" << endl;
+	cout << "Действия плохой погоды" << endl;
+	cout << "Ветер: " << WindCount << endl;
+	cout << "Дождь: " << RainCount << endl;
+
+
+
+	return 0;
+}
