5894a41636f75e4e05c87bc118c84d180c206e3f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-16 02:21:15 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/3.3/exercise_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <windows.h> // подключение заголовочного файла для SetConsoleCP() и SetConsoleOutputCP() 
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);			// функция задает входную кодовую страницу
+	SetConsoleOutputCP(1251);	// Задает выходную кодовую страницу, используемую консолью, связанной с вызывающим процессом
+
+	string nameUser;
+	string raceUser;
+
+	cout << "Введите имя персонажа: ";
+	cin >> nameUser;
+
+	cout << "Введите расу персонажа: ";
+	cin >> raceUser;
+
+	cout << "Родился новый " << raceUser
+		<< ", его зовут " << nameUser << ". ";
+	cout << "Доброй пожаловать в этот суровый мир!\n";
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/3.3/exercise_2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <windows.h> // подключение заголовочного файла для SetConsoleCP() и SetConsoleOutputCP() 
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	SetConsoleCP(1251);			// функция задает входную кодовую страницу
+	SetConsoleOutputCP(1251);	// Задает выходную кодовую страницу, используемую консолью, связанной с вызывающим процессом
+
+	string textNameUser;
+	string textPasswordUser;
+
+	cout << "Введите логин: ";
+	cin >> textNameUser;
+
+	cout << "Введите пароль: ";
+	cin >> textPasswordUser;
+
+	cout << "-----\n";
+
+	cout << textNameUser << ", вы успешно зашли!\n";
+
+	return 0;
+}
