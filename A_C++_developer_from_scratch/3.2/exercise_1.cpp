fe7b7c6fd807d7a0a209447f06f0a813a01193f7
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-16 02:19:45 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/3.2/exercise_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int a, square;
+
+	cout << "Введите число: ";
+	cin >> a;
+
+	square = a * a;
+
+	cout << "Квадрат числа " << a << ": " << square;
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/3.2/exercise_2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main() 
+{
+	setlocale(LC_ALL, "RUS");
+
+	int productCost;	
+	int deliveryCost;
+	int discount;		
+
+	std::cout << "Введите стоимость товара: ";
+	std::cin >> productCost;
+
+	std::cout << "Введите стоимость доставки: ";
+	std::cin >> deliveryCost;
+
+	std::cout << "Введите скидку от суммы: ";
+	std::cin >> discount;
+
+	int price = productCost + deliveryCost - discount;
+
+	std::cout << "\nСтоимость товара: " << productCost << "\n";
+	std::cout << "Cтоимость доставки: " << deliveryCost << "\n";
+	std::cout << "Скидка: " << discount << "\n";
+	std::cout << "---------\n";
+	std::cout << "Итого: " << price << "\n";
+
+}
