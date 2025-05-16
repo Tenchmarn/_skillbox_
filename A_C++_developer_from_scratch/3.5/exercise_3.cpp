92b51dd65422320883ee382ee4f5bb5108886352
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-16 20:25:12 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/3.5/exercise_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int beginHeight = 100;	// начальная высота бамбука.
+	int dailyGrowth = 50;	// ежедневный рост;
+	int nightFade = 20;		// размер съедаемого гусеницами бамбука за одну ночь;
+
+	cout << "\n\tПодсчет высоты бамбука к середине третьего дня.\n";
+	cout << "\nПараметры:\n";
+	cout << "\nРазмер съедаемого гусеницами бамбука за одну ночь: " << nightFade << " сантиметров.";
+	cout << "\nНачальная высота бамбука: " << beginHeight << " сантиметров.";
+	cout << "\nЕжедневный рост: " << dailyGrowth << " сантиметров." << "\n";
+
+	int calculationResult = beginHeight + (dailyGrowth - nightFade) * 2 + dailyGrowth / 2;
+
+	cout << "\nПодсчет высоты бамбука к половине третьего дня: " << calculationResult << " сантиметров.\n";
+
+	return 0;
+}
