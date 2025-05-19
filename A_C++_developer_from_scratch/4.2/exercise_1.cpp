b19e1a02a27ec6f52161d5c176a7cde042de9b9e
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-19 17:38:29 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/4.2/exercise_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	cout << "\n\tВы покупаете 3 товара в магазине.\n";
+	cout << "\n\tЕсли сумма вашего чека превышает 10 000 руб, \n\tвам будет сделана скидка 10%\n\n";
+
+	int product_1, product_2, product_3, discount = 10, amount = 0;
+
+	cout << "Введите стоимость товара 1: ";
+	cin >> product_1;
+	amount += product_1;
+
+	cout << "Введите стоимость товара 2: ";
+	cin >> product_2;
+	amount += product_2;
+
+	cout << "Введите стоимость товара 3: ";
+	cin >> product_3;
+	amount += product_3;
+
+	
+
+	if (amount >= 10000)
+	{
+		cout << "\n\tВам сделана скидка!\n";
+		amount = amount - ((amount / 100) * discount);
+	}
+	
+	cout << "\n\tСумма вашего чека: " << amount << '\n';
+
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/4.2/exercise_2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	cout << "Автомобиль выехал из Москвы в Рязань.\n";
+	cout << "Cумеет ли добраться автомобиль до Рязани за два часа?\n";
+	cout << "Расстояние между городами - 200 км.\n";
+
+	int average_speed;
+
+	cout << "\nВведите среднюю скорость движения автомобиля (км/ч): ";
+	cin >> average_speed;
+
+	average_speed *= 2;
+
+	if (average_speed >= 200)
+	{
+		cout << "\n\tВы проехали";
+	}
+	cout << "\nПройденное расстояние: " << average_speed << " километров.\n";
+
+
+	return 0;
+}
