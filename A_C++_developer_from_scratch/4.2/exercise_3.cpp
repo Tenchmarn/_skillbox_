a902dcdbb443c5a42852e538a8617b16d0ceca48
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-19 17:38:58 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/4.2/exercise_3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	cout << "Программа высчитывает модуль числа.\n";
+
+	int x;
+
+	cout << "Введите число: ";
+	cin >> x;
+
+	if (x >=0)
+	{
+		cout << "Модуль числа " << x << " равен " << x;
+	}
+	if (x < 0)
+	{
+		x = -x;
+		cout << "Модуль числа -" << x << " равен " << x;
+	}
+	
+	return 0;
+}
