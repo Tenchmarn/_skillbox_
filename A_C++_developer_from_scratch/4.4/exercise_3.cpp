2684c3782f9326aaa35a66a2b8d5bde9782706ec
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-20 16:15:13 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/4.4/exercise_3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int number;
+
+	cout << "Введите число: ";
+	cin >> number;
+
+	cout << "-----Проверяем-----";
+
+
+	if (number % 2 == 0)
+	{
+		cout << "\nЧисло " << number << " - чётное";
+	}
+	else
+	{
+		cout << "\nЧисло не чётное!";
+	}
+
+	cout << '\n';
+
+	return 0;
+}
