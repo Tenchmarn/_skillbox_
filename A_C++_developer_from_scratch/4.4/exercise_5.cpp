3d447c7e1da63d3dc4d71682138641858e724aff
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-20 16:15:36 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/4.4/exercise_5.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int number_1, number_2, remains = 0;
+
+	cout << "Введите первое число: ";
+	cin >> number_1;
+
+	cout << "Введите второе число: ";
+	cin >> number_2;
+
+	cout << "-----Проверяем-----\n";
+
+	if (number_1 % number_2 == 0)
+	{
+		cout << "Да, " << number_1 << " делится на " << number_2 << " без остатка!";
+	}
+	else
+	{
+		cout << "Нет, " << number_1 << " не делится на " << number_2 << " без остатка!";
+	}
+
+	cout << '\n';
+
+	return 0;
+}
