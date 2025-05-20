7a78bbe767bcbf3a5e52ac80bc2759f0fb3548f7
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-20 16:15:24 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/4.4/exercise_4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int experience, level = 1;
+
+	cout << "Введите число очков опыта: ";
+	cin >> experience;
+
+	cout << "-----Считаем-----";
+
+	if (experience < 1000)
+	{
+		level = 1;
+	}
+	else if (experience < 2500)
+	{
+		level = 2;
+	}
+	else
+	{
+		level = 3;
+	}
+
+	cout << "\nВаш уровень: " << level;
+
+	return 0;
+}
