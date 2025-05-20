bfa6b03675aac0430d53e5db2cbbb6625e751c73
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-20 16:14:20 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/4.4/exercise_1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int x_1, x_2;
+
+	cout << "Введите первое число: ";
+	cin >> x_1;
+
+
+	cout << "Введите второе число: ";
+	cin >> x_2;
+
+	cout << "-----Проверяем-----";
+
+	if (x_1 < x_2)
+	{
+		cout << "\nНаименьшее число: " << x_1;
+	}
+	else if (x_1 == x_2)
+	{
+		cout << "\nЧисла равны!";
+	}
+	else
+	{
+		cout << "\nНаименьшее число: " << x_2;
+	}
+
+	cout << '\n';
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/4.4/exercise_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int number_1, number_2, amount, check;
+
+	cout << "Введите первое число: ";
+	cin >> number_1;
+
+	cout << "Введите второе число: ";
+	cin >> number_2;
+
+	amount = number_1 + number_2;
+
+	cout << "Введите сумму: ";
+	cin >> check;
+
+	cout << "-----Проверяем-----";
+
+	if (amount == check)
+	{
+		cout << "\nВерно!";
+	}
+	else
+	{
+		cout << "\nОшибка! Верный результат: " << amount;
+	}
+
+	return 0;
+}
