5a1968bda8f7adaac387222b5dc8366464d97b10
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-23 01:35:05 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/5.2/exercise_1.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int x, y;
+	string quarter;
+
+	cout << "Введите координату Х: ";
+	cin >> x;
+
+	cout << "Введите координату У: ";
+	cin >> y; 
+
+	if ((x == 0) || (y == 0))
+	{
+		if (x == 0 && y > 0)
+		{
+			quarter = "граница 1 и 2 четверти";
+		}
+		else if (x == 0 && y < 0)
+		{
+			quarter = "граница 3 и 4 четверти";
+		}
+		else if (y == 0 && x > 0)
+		{
+			quarter = "граница 1 и 4 четверти";
+		}
+		else if (y == 0 && x < 0)
+		{
+			quarter = "граница 3 и 2 четверти";
+		}
+		else if ((x == 0) && (y == 0))
+		{
+			quarter = "пересечение четвертей!";
+		}
+		else
+		{
+			cout << "Ошибка!";
+		}
+	}
+	else if ((x != 0) && (y != 0))
+	{
+		if (x > 0 && y > 0)
+		{
+			quarter = "1 четверть";
+		}
+		else if (x < 0 && y > 0)
+		{
+			quarter = "2 четверть";
+		}
+		else if (x > 0 && y < 0)
+		{
+			quarter = "4 четверть";
+		}
+		else if (x < 0 && y < 0)
+		{
+			quarter = "3 четверть";
+		}
+		else
+		{
+			cout << "Ошибка!";
+		}
+	}
+
+	cout << "Точка координат: " << quarter << " - (" << x << '.' << y << ")";
+
+	cout << '\n';
+
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/5.2/exercise_2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int limit = 100000;		// лимит снятия 
+	int withdrawal = 100;	// выдача банкнот по курсу
+	int user_withdrawal;	// пользовательский вывод валюты
+	int withdrawal_amount;  // сумма вывода средств
+	
+
+	cout << "Лимит по снятию: " << limit << '\n';
+	cout << "Выдача по: " << withdrawal << '\n';
+	cout << "Введите сумму снятия: ";
+	cin >> user_withdrawal;		
+
+	withdrawal_amount = user_withdrawal / withdrawal; 
+	if (user_withdrawal < 100000 && user_withdrawal > 0)
+	{
+		if (user_withdrawal % withdrawal == 0) 
+		{
+			cout << "Вывод количества купюр: " << user_withdrawal / withdrawal;
+		}
+		else if (withdrawal_amount > 0)
+		{
+			cout << "Ошибка! Банкомат может выдать только: " << withdrawal_amount * withdrawal;
+		}
+	}
+	else
+	{
+		cout << "Ошибка!";
+	}
+
+
+	cout << '\n';
+
+	return 0;
+}
