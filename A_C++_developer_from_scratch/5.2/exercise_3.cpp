3f16e7d55312e48e85e2d40a750ec38541822bf0
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-23 01:35:24 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/5.2/exercise_3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int year, days;
+	string type_year;
+
+	cout << "Введите год: "; cin >> year;
+
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 == 0)
+			{
+				type_year = " - высокосный";
+				days = 366;
+			}
+			else
+			{
+				days = 365;
+				type_year = " - невысокосный";
+			}
+		}
+		else
+		{
+			type_year = " - высокосный";
+			days = 366;
+		}
+	}
+	else
+	{
+		days = 365;
+		type_year = " - невысокосный";
+	}
+
+	cout << "Год" << type_year << " и в нем: " << days << " дней.";
+
+	cout << '\n';
+	return 0;
+}
