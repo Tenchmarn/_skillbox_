c84da0b621bf9ccd97548f6ca6342c02972f125f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-23 04:29:45 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/5.3/exercise_4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int a, b, c;
+	int counter = 0; // счетчик
+
+
+	cout << "Введите число 1: ";
+	cin >> a; 
+
+	cout << "Введите число 2: ";
+	cin >> b; 
+
+	cout << "Введите число 3: ";
+	cin >> c; 
+
+	if ((a == b) && (a == c)) counter = 3;
+	else if ((a == b) && (a != c)) counter = 2;
+	else if ((a != b) && (a == c)) counter = 2;
+	else if (b == c) counter = 2;
+	else counter = 0;
+
+	cout << "\n\tОтвет: " << counter << " совпадающих.";
+
+	cout << '\n';
+
+	return 0;
+}
