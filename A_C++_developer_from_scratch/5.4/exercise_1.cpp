18af47c5889185aefd287fd3847769edfe36cbee
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-27 12:21:21 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/5.4/exercise_1.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int height, weight, hands = 2, legs = 2; // рост, вес, руки, ноги
+	int experience; // опыт полета
+	string colour_eyes; // цвет глаз
+	int counter = 0; // счетчик приема
+
+	cout << "Введите рост: ";
+	cin >> height;
+
+	cout << "Введите вес: ";
+	cin >> weight;
+
+	cout << "Введите количество рук: ";
+	cin >> hands;
+
+	cout << "Введите количество ног: ";
+	cin >> legs;
+
+	cout << "Введите опыт полета (часы): ";
+	cin >> experience;
+
+	cout << "Введите цвет глаз: ";
+	cin >> colour_eyes;
+
+	if (height >= 145 || height <= 165)
+	{
+		counter++;
+	}
+	if (weight >= 45 || weight <= 65)
+	{
+		counter++;
+	}
+	if (hands == 2)
+	{
+		counter++;
+	}
+	if (legs == 2)
+	{
+		counter++;
+	}
+	if (experience >= 100)
+	{
+		counter++;
+	}
+	if (colour_eyes != "зеленый")
+	{
+		counter++;
+	}
+
+	if (counter == 6)
+	{
+		cout << "Годен!";
+	}
+	else
+	{
+		cout << "Не годен!";
+	}
+
+	cout << '\n';
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/5.4/exercise_2.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int x, y;
+	int counter = 0;
+
+	cout << "Введите ширину отверстия: ";
+	cin >> x;
+
+	cout << "Введите высоту отверстия: ";
+	cin >> y;
+
+	int x_1, y_1, z_1;
+
+	cout << "\n\tРазмер кирпича.\n";
+
+	cout << "Введите ширину: ";
+	cin >> x_1;
+
+	cout << "Введите высоту: ";
+	cin >> y_1;
+
+	cout << "Введите длину: ";
+	cin >> z_1;
+
+
+	if (x >= x_1 && y >= y_1)
+	{
+		counter++;
+	}
+	else if (x >= x_1 && y >= z_1)
+	{
+		counter++;
+	}
+	else if (x >= y_1 && x >= z_1)
+	{
+		counter++;
+	}
+	else
+	{
+		cout << "Ошибка!";
+	}
+
+
+	if (counter == 1)
+	{
+		cout << "YES\n";
+	}
+	else
+	{
+		cout << "NO\n";
+	}
+
+
+	return 0;
+}
