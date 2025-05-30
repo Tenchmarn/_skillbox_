6fd0dcb8e903a7c910e7d5730cfe0111a3ee90a9
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-05-30 19:55:29 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/5.6/exercise_1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int speed, height; // скорость, высота
+
+	cout << "Введите скорость (км/ч): ";
+	cin >> speed;
+
+	cout << "Введите высоту: ";
+	cin >> height;
+
+
+	if ((speed >= 750 && speed <= 850) && (height >= 9000 && height <= 9500))
+	{
+		cout << "ДА!";
+	}
+	else
+	{
+		cout << "Нет";
+	}
+
+	cout << '\n';
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/5.6/exercise_2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int day_number;
+    cout << "Введите день: ";
+    cin >> day_number;
+
+    if (day_number >= 1 && day_number <= 31)
+    {
+
+        if ((day_number >= 1 && day_number <= 5)
+            || (day_number >= 8 && day_number <= 10)
+            || (day_number % 7 == 6)
+            || (day_number % 7 == 0))
+        {
+            cout << "Выходной день!";
+        }
+        else
+        {
+            cout << "Рабочий день.";
+        }
+    }
+    else
+    {
+        cout << "Ошибка! Введён неверный день.";
+    }
+
+    return 0;
+}
+
+
