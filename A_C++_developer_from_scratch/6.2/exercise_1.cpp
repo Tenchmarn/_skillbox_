fd89f77cd9ed92558921370faed7c3a1838d2f7c
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-04 13:14:28 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/6.2/exercise_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	SetConsoleCP(1251);			// функция задает входную кодовую страницу
+	SetConsoleOutputCP(1251);	// Задает выходную кодовую страницу, используемую консолью, связанной с вызывающим процессом
+
+	string answer = "";
+
+	while (answer != "Да, конечно, сделал")
+	{
+		cout << "Вы выполнили задание?\n";
+		getline(std::cin, answer);
+	}
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/6.2/exercise_2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	SetConsoleCP(1251);			// функция задает входную кодовую страницу
+	SetConsoleOutputCP(1251);	// Задает выходную кодовую страницу, используемую консолью, связанной с вызывающим процессом
+
+	string answer_user_1 = "";
+	string answer_user_2 = "";
+
+	cout << "Привет, как тебя зовут?\n";
+	cin >> answer_user_1;
+
+	cout << answer_user_1 << " купи слона!\n";
+
+	while (true)
+	{
+		cin >> answer_user_2;
+		cout << "Все говорят \"" << answer_user_2
+			<< "\", а ты купи слона!\n";
+	}
+
+	return 0;
+}
