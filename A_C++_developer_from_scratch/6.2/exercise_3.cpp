f607818431d24ee2e1d3a559759b8a0dc42b5344
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-04 13:15:06 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/6.2/exercise_3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n, k;
+
+	cout << "Введите число k: ";
+	cin >> k;
+
+	cout << "Введите степень числа k, n: ";
+	cin >> n;
+
+	int power = 1, i = 1;
+
+	while (i<=n)
+	{
+		power *= k;
+		cout << k << '^' << i << " = " << power << '\n';
+		cout << '\n';
+		i++;
+	}
+
+	cout << '\n';
+
+	return 0;
+}
