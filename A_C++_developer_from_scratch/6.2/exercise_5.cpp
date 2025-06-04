10d7ad56ee90baecc4082fe565ca855c7a02a9cd
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-04 13:15:29 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/6.2/exercise_5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n, sum = 0, count = 1;
+
+	cout << "\tВведите пин-код из 4-х цифр.\n";
+
+	do
+	{
+		if (count == 5) 
+		{
+			count = 1;
+			sum = 0;
+			cout << "\n\tВвод некорректный, попробуйте еще раз\n";
+		}
+
+		while (count != 5)
+		{
+			cout << "Введите цифру " << count << " пин-кода: ";
+			cin >> n;
+
+			sum += n;
+			count++;
+		}
+	} while (sum != 42);
+
+	cout << "\n\tВвод корректный.\n";
+
+	return 0;
+}
