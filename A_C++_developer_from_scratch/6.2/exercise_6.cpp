bf83d58eb5164f09edf0e12bb21f4f113f51d0c4
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-04 13:15:41 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/6.2/exercise_6.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n, sum = 0, count_1;
+
+	cout << "Введите количество для сложения: ";
+	cin >> count_1;
+
+	do
+	{
+		cout << "Введите цифру или число "
+			<< count_1
+			<< ": ";
+		cin >> n;
+
+		sum += n;
+
+		count_1--;
+
+	} while (count_1 != 0);
+
+	cout << "\nРезультат сложения: " << sum;
+
+	return 0;
+}
