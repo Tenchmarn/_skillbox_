70dbb76c5bd8e4b95d58961a704646296c646968
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-05 03:36:35 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/6.3/exercise_4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace  std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int count, reversed_number = 0;
+	int remainder;
+
+	cout << "Введите целое число: ";
+	cin >> count;
+
+	while (count != 0)
+	{
+		remainder = count % 10;
+		reversed_number = reversed_number * 10 + remainder;
+		count /= 10;
+	}
+
+	cout << "Обратное число: " << reversed_number << endl;
+
+	return 0;
+}
