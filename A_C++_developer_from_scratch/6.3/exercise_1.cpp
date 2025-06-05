681a19ec66d6b391017c2a36a73eceb6cd5fddf2
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-05 03:35:50 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/6.3/exercise_1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n, sum = 0, count_1 = 0;
+
+	do
+	{
+		cout << "Введите пин-код любой длинны из цифр: ";
+		cin >> n; // 987855
+
+		while (n != 0)
+		{
+			sum += n % 10;
+			n /= 10;
+		}
+
+		if (sum != 42)
+		{
+			cout << "\tВведите число еще раз.\n";
+			sum = 0;
+		}
+
+	} while (sum != 42);
+
+	cout << "\n\tВвод корректный.\n";
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/6.3/exercise_2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int i = 1, a, count = 1;
+
+	cout << "Введите площадь: ";
+	cin >> a; // 49
+
+	while (count < a)
+	{
+		count = i * i;
+		i++;
+	}
+
+	if (count == a)
+	{
+		cout << "Скидка есть! Точный квадрат.";
+	}
+	else
+	{
+		cout << "Скидки нет. Квадрат не точный!";
+	}
+
+	return 0;
+}
