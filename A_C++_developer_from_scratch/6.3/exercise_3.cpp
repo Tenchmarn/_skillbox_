6ae07274ed57e4f260003ff6b305a4367e9aee1f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-05 03:36:23 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/6.3/exercise_3.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int number, count_1 = 0;
+
+	cout << "Введите число состоящее только из 0 или 1: ";
+	cin >> number;
+
+	int zeros = 0;
+	int ones = 0;
+
+	if (number == 0)
+	{
+		cout << "Число состоит только из нулей.\n";
+		return 0;
+	}
+
+	while (number > 0)
+	{
+		int digit = number % 10;
+
+		if (digit == 0) 
+		{
+			zeros++;
+		}
+		else if (digit == 1) 
+		{
+			ones++;
+		}
+		else 
+		{
+			cout << "Число содержит цифры, отличные от 0 и 1" << endl;
+			return 0;
+		}
+		number /= 10;
+	}
+
+	if (zeros == 0) 
+	{
+		cout << "Число состоит только из единиц" << endl;
+	}
+	else if (ones == 0) 
+	{
+		cout << "Число состоит только из нулей" << endl;
+	}
+	else 
+	{
+		cout << "Число содержит и нули, и единицы" << endl;
+	}
+
+
+	return 0;
+}
