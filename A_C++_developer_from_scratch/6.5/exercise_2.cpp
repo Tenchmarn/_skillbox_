7d50bac44797d5845a532ff8d09b003a92d57087
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-09 00:55:52 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/6.5/exercise_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int number;
+
+	cout << "Введите число: ";
+	cin >> number;
+
+	int count_1 = (number) ? 0 : 1;
+
+	while (number)
+	{
+		number /= 10;
+		count_1++;
+	}
+
+	cout << "Цифр в числе: " << count_1;
+
+	return 0;
+}
