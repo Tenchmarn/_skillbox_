719097f6aa1899464c6e03aa9b6318e8f54bdb91
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-09 00:57:07 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/6.5/exercise_5.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int number, variable_1 = 1, variable_2 = 0, next_term = 0;
+	int count = 1;
+
+	cout << "Введите номер последовательности: ";
+	cin >> number;
+
+	while (count <= number)
+	{
+		next_term = variable_1 + variable_2;
+		variable_1 = variable_2;
+		variable_2 = next_term;
+		count++;
+	}
+	
+	cout << "Число в последовательности: " << next_term << '\n';
+
+	return 0;
+}
+
