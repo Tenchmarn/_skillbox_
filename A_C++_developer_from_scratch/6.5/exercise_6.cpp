1b56311a73aedb8d04f7c4fe08c744d8accc92b6
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-09 00:57:17 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/6.5/exercise_6.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int contribution;			// вклад
+	int percent;				// процентная ставка
+	int desired_amount;			// желаемая сумма
+	int year_count = 0;			// счетчик лет
+
+	cout << "Введите размер вклада: ";
+	cin >> contribution;
+
+	cout << "Введите процентную ставку: ";
+	cin >> percent;
+
+	cout << "Введите желаемую сумму: ";
+	cin >> desired_amount;
+
+	while (contribution < desired_amount)
+	{
+		int interest = (contribution * percent) / 100;
+		contribution += interest;
+
+		year_count++;
+	}
+
+	cout << "Придётся подождать: " << year_count << " лет\n";
+
+	return 0;
+}
