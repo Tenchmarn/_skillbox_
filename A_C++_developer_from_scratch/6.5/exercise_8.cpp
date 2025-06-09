bd3f0d99c5ee52af025158825e07f0e72ea18d44
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-09 12:59:37 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/6.5/exercise_8.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string answer = " ";
+
+	int low = 0;
+	int high = 63;
+
+	cout << "Загадайте число в диапазоне от 0 до 63.\n";
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2; 
+
+        if (low == high) {
+            cout << "Ваше число: " << low << "!\n";
+            break;
+        }
+
+        cout << "Ваше число больше " << mid << "? (да/нет): ";
+        cin >> answer;
+
+        if (answer == "да") {
+            low = mid + 1;
+        }
+        else if (answer == "нет") {
+            high = mid;
+        }
+        else {
+            cout << "Пожалуйста, отвечайте только 'да' или 'нет'.\n";
+        }
+    }
+
+	return 0;
+}
