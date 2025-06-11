fc9cf507b2a049020ac95e2d6bf4c276352c16c1
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-12 00:31:39 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/7.3/exercise_2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int count = 1, number, count_rezul = 1;
+
+	cout << "Введите площадь: ";
+	cin >> number;
+
+	if (number < 0)
+	{
+		cout << "Ввод неправильный!\n";
+		return 0;
+	}
+
+	for (count, count_rezul; count_rezul < number; count++)
+		count_rezul = count * count;
+
+	if (count_rezul == number) cout << "Скидка есть! Точный квадрат.\n";
+	else cout << "Скидки нет. Квадрат не точный!\n";
+
+	return 0;
+}
