41f3ebe3525d702506bc8b696539d10eb62e034d
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-12 00:31:56 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/7.3/exercise_3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int count = 10;			// счетчик
+	int attempt = 1;		//попытки
+
+	string answer = " ";
+
+	cout << "Необходимо обезвредить бомбу! У Вас 10 попыток!\n";
+
+	for (count; count > 0; attempt++)
+	{
+		count--;
+		cout << "\n\tВведите команд(ы)\n\"да\" - обезвредить;\n"
+			<< "\"Нет\" - не обезвреживать;\n";
+		cout << "Ваш ответ: "; cin >> answer;
+		cout << "\n";
+
+		if (count == 0) 
+		{
+			cout << "\n\tБомба взорвалась!\n";
+			return 0;
+		}
+		else if (answer == "да")
+		{
+			cout << "\tБомба обезврежена!\n";
+			cout << "Вам оставалось " << count << " попыток.\n";
+			break;
+		}
+		else if (answer == "нет") cout << "\n\t\tСчетчик тикает. У Вас остается мало попыток!\n";
+		else cout << "\t\tКоманда неизвестна. У Вас осталось " << count << " попыток!";
+	}
+
+	cout << "Количество ваших попыток составляло: "
+		<< attempt << '\n';
+
+	return 0;
+}
