27f93d660ce92f08974191cda9f38d99a3416c2f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-11 23:23:54 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/7.2/exercise_4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int number;
+	int sum = 0;
+
+	cout << "Введите число: ";
+	cin >> number;
+
+	for (number; number != 0; number /= 10)
+	{
+		if ((number % 10) % 2 != 0)
+		{
+			sum += number % 10;
+		}
+	}
+
+	cout << "Сумма цифр равна: " << sum << '\n';
+
+	return 0;
+}
