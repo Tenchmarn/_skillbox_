de0b7a413dc1d419f2905adc93096f9c9015f89f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-11 23:23:15 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/7.2/exercise_1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	SetConsoleCP(1251);			// функция задает входную кодовую страницу
+	SetConsoleOutputCP(1251);	// Задает выходную кодовую страницу, используемую консолью, связанной с вызывающим процессом
+
+
+	string answer = "Ку-КУ!\n";
+	int hour;
+
+
+	cout << "Введите, который час: ";
+	cin >> hour;
+
+	if (hour <= 0 || hour > 24)
+	{
+		cout << "Ошибка, таких часов не существует!\n";
+		return 0;
+	}
+
+	cout << '\n';
+
+	for (hour; hour > 0; hour--) cout << answer;
+
+	cout << '\n';
+
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/7.2/exercise_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int number;
+	int sum = 0;
+
+	cout << "Введите число: ";
+	cin >> number;
+
+	for (number; number != 0; number /= 10)
+	{
+
+		sum += number % 10;
+
+	}
+
+	cout << "Сумма цифр равна: " << sum << '\n';
+
+	return 0;
+}
