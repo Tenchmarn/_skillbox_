b2044aa64e31ac4ed704d357571165bce97f9b8d
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-11 23:24:10 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/7.2/exercise_5.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	SetConsoleCP(1251);			// функция задает входную кодовую страницу
+	SetConsoleOutputCP(1251);	// Задает выходную кодовую страницу, используемую консолью, связанной с вызывающим процессом
+
+	string answer = "";
+	int count = 0;
+
+	for (count; answer != "Да, конечно, сделал"; count++)
+	{
+		cout << "Вы выполнили задание?\n";
+		getline(std::cin, answer);
+	}
+
+	int result = count % 100;
+
+	cout << "\nНу почему тебя нужно спрашивать "
+		<< count << ((result % 10 >= 2 && result % 10 <= 4 && (result < 11 || result > 14)) ? " раза???" : " раз???")
+		<< '\n';
+
+	return 0;
+}
