af33f6602f687d1b7aff4370c6fac71b424cbba2
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-11 23:23:44 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/7.2/exercise_3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int number;
+
+	cout << "Введите число для умножения: ";
+	cin >> number;
+
+	cout << '\n';
+
+	for (int count = 0; count <= 10; count++) 
+		cout << number << '*' << count << " = " << number * count << '\n';
+
+	cout << '\n';
+
+	return 0;
+}
