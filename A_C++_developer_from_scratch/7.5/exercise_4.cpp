26cfc875f2ab4dba3e3eb85fd65d1315f0d9a821
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-09 08:49:08 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/7.5/exercise_4.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int width, height;
+
+	cout << "Введите ширину и высоту рамки: ";
+	cin >> width >> height;
+
+	for (int j = 1; j <= height; j++)
+	{
+		cout << '|';
+		for (int i = 1; i <= width - 2; i++)
+		{
+			if ((j == 1) || (j == height))
+			{
+				cout << '-';
+			}
+			else
+			{
+				cout << ' ';
+			}
+		}
+		cout << '|' << '\n';
+	}
+
+	return 0;
+}
