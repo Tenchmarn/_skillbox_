18aa2c687ef00ff8a635245a68eb5604e88b654d
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-09 08:48:52 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/7.5/exercise_3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int x = 6, y = 15;
+	char ch = ' ';
+
+	while (1)
+	{
+		
+		cout << "[Марсоход]: " << "Марсоход находится на позиции "
+			<< x << ", " << y << ", введите команду: \n";
+		cout << "[Оператор]: "; cin >> ch;
+		if (ch == 'w') y++;
+		if (ch == 'a') x--;
+		if (ch == 's') y--;
+		if (ch == 'd') x++;
+		
+		if (x > 15) x--;
+		if (x < 0) x++;
+		if (y > 20) y--;
+		if (y < 0) y++;
+			
+	}
+
+	return 0;
+}
