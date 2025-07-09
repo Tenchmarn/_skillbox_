87c19be0dd172aef80d945c260099bcdd0d1ad80
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-09 08:48:39 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/7.5/exercise_1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int stock = 100; 
+	int month; 
+	int count_month;
+
+	cout << "Введите месяц: ";
+	cin >> month;
+	cout << '\n';
+
+	for (count_month = 1; count_month <= month; count_month++)
+	{
+		stock = stock - 4;
+
+		cout << "После " << count_month
+			<< " месяца у вас в запасе останется "
+			<< stock << " кг гречки\n";
+		if (stock <= 0)
+		{
+			cout << "\n\tПосле " << count_month
+				<< " месяца гречка закончится\n";
+			break;
+		}
+		else if (stock > 0 && count_month == month)
+		{
+			cout << "\n\tПосле " << count_month
+				<< " месяца гречка останется\n";
+		}
+	}
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/7.5/exercise_2.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int choice;						// выбор
+	int water;						// вода
+	int milk;						// молоко
+	int count_americano = 0;		// счетчик_1
+	int count_latte = 0;			// счетчик_2
+
+
+	cout << "Введите количество воды в мл: ";
+	cin >> water;		// 700
+
+	cout << "Введите количество молока в мл: ";
+	cin >> milk;		// 340
+
+	do {
+		cout << "\nВыберите напиток (1 - американо, 2 - латте): ";
+		cin >> choice;
+
+		int count = 0;
+
+		if (choice == 1)
+		{
+			if (water >= 300)
+			{
+				cout << "\n\tВаш напиток готов!\n\n";
+				water -= 300;
+				count_americano++;
+			}
+			else
+			{
+				cout << "\n\tНе хватает воды!\n";
+			}
+		}
+		else if (choice == 2)
+		{
+			if (water >= 30)
+			{
+				if (milk > 270)
+				{
+					cout << "\n\tВаш напиток готов!\n\n";
+					water -= 30;
+					milk -= 270;
+					count_latte++;
+				}
+				else
+				{
+					cout << "\n\tНе хватает молока!\n";
+				}
+			}
+			else
+			{
+				cout << "\n\tНе хватает воды!\n";
+			}
+		}
+		else
+		{
+			cout << "\n\tОшибка чтения символа!\n";
+		}
+		
+		if (water < 300)
+		{
+			if (milk < 270)
+			{
+				cout << "\n***Отчет***\n";
+				cout << "Ингридиентов осталось: ";
+				cout << "\n\tВода: " << water;
+				cout << "\n\tМолоко:" << milk;
+				cout << "\nКружек американо приготовлено: " << count_americano;
+				cout << "\nКружек латте приготовлено: " << count_latte;
+				cout << endl;
+				break;
+			}
+		}
+
+	} while (1);
+
+	return 0;
+}
