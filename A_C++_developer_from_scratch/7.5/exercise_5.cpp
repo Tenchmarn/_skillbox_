7a346b065c825d49cf0c4cb67bacacf4fb9d90ba
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-09 23:04:23 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/7.5/exercise_5.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int width = 20;
+	int height = 40;
+	char ch = '^';
+
+	for (int i = 0; i <= width; i++)
+	{
+		for (int j = 0; j <= height; j++)
+		{
+			if (((height / 2) == j) && ((width /2) != i))
+			{
+				cout << ch;
+				ch = '|';
+			}
+			else if ((width / 2) == i)
+			{
+				if ((height / 2) == j)
+				{
+					cout << '+';
+				}
+				else if (j == height)
+				{
+					cout << '>';
+				}
+				else if(j < height)
+				{
+					cout << '-';
+				}
+			}
+			else
+			{
+				cout << ' ';
+			}
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
