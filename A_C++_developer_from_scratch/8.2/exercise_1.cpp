47815cc2862f9c119640c8e81e254ae47e4d1e27
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-17 21:32:54 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/8.2/exercise_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int price;		// цена
+	float weight;	// вес для покупки
+	float result;	// результат
+
+	cout << "Введите цену за 100 грамм: ";
+	cin >> price;
+
+	cout << "Введите вес товара: ";
+	cin >> weight;
+
+	result = (weight / 100) * price;
+
+	cout << "Цена покупки: " << result << '\n';
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/8.2/exercise_2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+    float protein_per_100, carbs_per_100, weight;
+
+    // Ввод данных
+    cout << "Введите протеин на 100 грамм: ";
+    cin >> protein_per_100;
+
+    cout << "Введите углеводы на 100 грамм: ";
+    cin >> carbs_per_100;
+
+    cout << "Введите массу: ";
+    cin >> weight;
+
+    // Расчёт
+    float protein = (protein_per_100 / 100.) * weight;
+    float carbs = (carbs_per_100 / 100.) * weight;
+
+    // Вывод
+    cout << "Белок: " << protein << " гр.\n";
+    cout  << "Углеводы: " << carbs << " гр.\n";
+
+	return 0;
+}
