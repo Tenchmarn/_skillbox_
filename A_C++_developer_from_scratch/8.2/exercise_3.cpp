1354be44c2fad6d832ca6d5af36f4281743baaa0
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-17 21:33:17 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/8.2/exercise_3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int price;		// цена
+	float weight;	// вес для покупки
+	float result;	// результат
+	int discount;	// скидка
+
+	cout << "Введите цену за 100 грамм: ";
+	cin >> price;
+
+	cout << "Введите вес товара: ";
+	cin >> weight;
+
+	cout << "Введите скидку на товар: ";
+	cin >> discount;
+
+	result = (weight / 100) * price;
+
+	if (discount > 0)
+	{
+		result = (result / 100) * discount;
+		cout << "Цена покупки с учетом скидки: " << result << '\n';
+	}
+	else if (discount <= 0)
+	{
+		cout << "Цена покупки: " << result << '\n';
+	}
+
+	return 0;
+}
