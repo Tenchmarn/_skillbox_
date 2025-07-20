f017466b3fb27aac71659aae6b74c9e7e1e83de9
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-20 13:59:34 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/8.3/exercise_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int input_chatls;
+
+	cout << "Введите количество чатлов: ";
+	cin >> input_chatls; // 5600
+
+	float kc = input_chatls / 4400.0;
+	int gravitsapa = input_chatls / 2200;
+
+	cout << input_chatls << " чатлов = " << kc << " КЦ" << endl;
+	cout << "Можно купить " << gravitsapa << " гравицап(ы)" << endl;
+
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/8.3/exercise_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	float x, y;
+
+	cout << "Введите местоположение фигуры." << '\n';
+
+	cout << "y = ";
+	do
+	{
+		cin >> y;
+		if (y > 0.81 || y < 0)
+		{
+			cout << "введите корректное значение y = ";
+		}
+	} while (y > 0.81 || y < 0);
+
+	cout << "x = ";
+	do
+	{
+		cin >> x;
+		if (x > 0.81 || x < 0)
+		{
+			cout << "введите корректное значение x = ";
+		}
+	} while (x > 0.81 || x < 0);
+
+	cout << "Фигура находиться в клетке ("
+		<< int(x * 10) << ", "
+		<< int(y * 10) << ")."
+		<< '\n';
+
+	return 0;
+}
