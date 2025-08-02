84a5ff09388724493a58b9567d9a4a095fae5fd2
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-02 08:42:35 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/8.4/exercise_3.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    float H = 0.f; // ввод данных (от земли до пола лифта)
+    float distance_between_floors = 3.975f; // расстояние между этажами
+    int count_floor = 1; // счетчик этажей
+
+    cout << "Введите высоту лифта (от земли до пола лифта): ";
+    cin >> H;
+
+    if (H < 0)
+    {
+        cout << "Ошибка, расстояние не может быть ниже нуля!" << '\n';
+        cout << "Перезапустите программу!" << '\n';
+        return 1;
+    }
+
+    count_floor = static_cast<int>((H + distance_between_floors / 2) / distance_between_floors) + 1;
+
+    cout << "Лифт на этаже " << count_floor << ".\n";
+
+    return 0;
+}
