8c0a1e896af430a0458e08cf48c37880169a87b9
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-02 08:42:52 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/8.4/exercise_4.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int lower_limit; //	нижний
+	int upper_limit; // верхний
+	float intermediate_point; // промежуточная точка
+	float brightness; // яркость
+
+	cout << "Введите яркость левой границы градиента: ";
+	cin >> lower_limit;
+
+	cout << "Введите правую границу градиента: ";
+	cin >> upper_limit;
+
+	cout << "Введите положение точки между границами: ";
+	cin >> intermediate_point;
+	
+	brightness = lower_limit + (upper_limit - lower_limit) * intermediate_point;
+
+
+	cout << "Яркость точки: " << brightness << '\n';
+
+	return 0;
+}
