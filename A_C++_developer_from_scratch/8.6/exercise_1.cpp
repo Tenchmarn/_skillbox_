a69e3152cf601e216f2b7a1b136ace5e8e679e5c
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-05 08:50:30 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/8.6/exercise_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	float m, F; // масса, сила тяги в ньютонах, секунды
+	int t;
+
+	cout << "Введите массу в кг: "; cin >> m;
+	cout << "Введите силу тяги в ньютонах: "; cin >> F;
+	cout << "Введите секунды: "; cin >> t;
+
+	float acceleration = F/m; // ускорение
+
+	float result = (acceleration * pow(t, 2))/2;
+
+	cout << "На " << result << " метра от первоначального положения.\n";
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/8.6/exercise_2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	float health;		// здоровье
+	float resistance;	// сопротивление
+	float damage;		// урон
+
+	cout << "Введите здоровье: "; cin >> health;
+	cout << "Введите сопротивление: "; cin >> resistance;
+	
+	while (health > 0)
+	{
+		cout << "Введите урон: "; cin >> damage;
+
+		float community;	// общий показатель урона
+		if (damage == 1)
+		{
+			community = damage - resistance;
+		}
+		else
+		{
+			community = damage * resistance;
+		}
+		
+		health -= community;
+
+		if (health < 0)
+		{
+			cout << "Орк побежден!\n";
+			break;
+		}
+		else
+		{
+			cout << "\nЖизнь орка: " << health;
+			cout << "\nОрку был нанесен урон: " << community;
+			cout << '\n';
+		}
+	}
+
+	return 0;
+}
