124fac55a47c3cb611537e812ec3c911e8633e60
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-19 21:46:48 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/9.3/exercise_3.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    std::string start, move;
+
+    std::cout << "Input start position and move position:\n";
+
+    std::cin >> start >> move;
+
+    {
+
+        if ((abs(start[0] - move[0]) == 1) && (abs(start[1] - move[1]) == 2)
+
+            || (abs(start[0] - move[0]) == 2) && (abs(start[1] - move[1]) == 1)
+
+            )
+
+            std::cout << "Horse move\n";
+
+        if (abs((start[0] - move[0]) == 0) && (abs(start[1] - move[1]) > 0)
+
+            || (abs(start[0] - move[0]) > 0) && (abs(start[1] - move[1]) == 0)
+
+            )
+
+            std::cout << "Rook move\n";
+
+        if ((abs(start[0] - move[0]) == abs(start[1] - move[1]))
+
+            && (abs(start[0] - move[0]) > 0)
+
+            )
+
+            std::cout << "Bishop move\n";
+
+        if (
+
+            (
+
+                (abs(start[0] - move[0]) == abs(start[1] - move[1]))
+
+                && (abs(start[0] - move[0]) > 0)
+
+                )
+
+            ||
+
+            (
+
+                abs((start[0] - move[0]) == 0)
+
+                && (abs(start[1] - move[1]) > 0)
+
+                ||
+
+                (
+
+                    abs(start[0] - move[0]) > 0)
+
+                && (abs(start[1] - move[1]) == 0)
+
+                )
+
+            )
+
+            std::cout << "Queen move\n";
+
+        if ((abs(start[0] - move[0]) <= 1
+
+            && abs(start[1] - move[1]) <= 1)
+
+            )
+
+            std::cout << "King move\n";
+
+    }
+
+}
