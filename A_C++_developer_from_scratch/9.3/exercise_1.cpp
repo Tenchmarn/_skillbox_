f210224d6b61ee4edffb06251aca955d8123c938
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-19 21:46:30 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/9.3/exercise_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+
+    char num1, op, num2;
+
+    cout << "Input expression:\n";
+    cin >> num1 >> op >> num2;
+
+
+
+    if (num1 < '0' || num1 > '9')
+    {
+        cout << "First number error\n";
+    }
+    else if (op != '*')
+    {
+        cout << "This is not multiplication\n";
+    }
+    else if (num2 < '0' || num2 > '9')
+    {
+        cout << "Second number error\n";
+    }
+    else 
+    {
+        int result = (num1 - '0') * (num2 - '0');
+
+        cout << "Result: " << result << "\n";
+    }
+
+}
---
+++ A_C++_developer_from_scratch/9.3/exercise_2.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char first, second;
+
+    cout << "Input two cards:\n";
+
+    cin >> first >> second;
+
+    if (first == second) 
+    {
+        cout << "Equal\n";
+    }
+    else 
+    {
+        int firstCardValue = 0;
+
+        if (first >= '6' && first <= '9') 
+        {
+            firstCardValue = first - '0';
+        }
+        else if (first == '0') 
+        {
+            firstCardValue = 10;
+        }
+        else if (first == 'J') 
+        {
+            firstCardValue = 11;
+        }
+        else if (first == 'Q') 
+        {
+            firstCardValue = 12;
+        }
+        else if (first == 'K') 
+        {
+            firstCardValue = 13;
+        }
+
+
+
+        int secondCardValue = 0;
+
+        if (second >= '6' && second <= '9') 
+        {
+            secondCardValue = second - '0';
+        }
+        else if (second == '0') 
+        {
+            secondCardValue = 10;
+        }
+        else if (second == 'J') 
+        {
+            secondCardValue = 11;
+        }
+        else if (second == 'Q') 
+        {
+            secondCardValue = 12;
+        }
+        else if (second == 'K') 
+        {
+            secondCardValue = 13;
+        }
+
+
+
+        if ((first == 'A' && second != '6') || (firstCardValue > secondCardValue))
+        {
+            std::cout << "First\n";
+        }
+        else 
+        {
+            std::cout << "Second\n";
+        }
+    }
+    return 0;
+
+}
