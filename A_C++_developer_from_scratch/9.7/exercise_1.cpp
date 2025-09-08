0cae4adb2cd0e395831ac30d35c3b61de46a3f23
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-08 08:29:30 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/9.7/exercise_1.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	string time_departure{ "00:00" };
+	string time_arrival{ "00:00" };
+
+	int hours_td(0),
+		minutes_td(0),
+		second_td(0);
+
+	int hours_ta(0),
+		minutes_ta(0),
+		second_ta(0);
+
+	int hours_result(0),
+		minutes_result(0),
+		second_result(0);
+
+	int second_day = 86400, time_second_result(0);
+
+	while (true)
+	{
+		cout << "Введите время отправления (HH:MM): ";
+		cin >> time_departure;
+
+		if ((time_departure.length() != 5)
+			|| (time_departure[2] != ':')
+			|| (time_departure[0] < '0')
+			|| (time_departure[0] > '2')
+			|| (time_departure[1] < '0')
+			|| (time_departure[1] > '3')
+			|| (time_departure[3] < '0')
+			|| (time_departure[3] > '5')
+			|| (time_departure[4] < '0')
+			|| (time_departure[4] > '9'))
+		{
+			cout << "Error! Try again! (Ошибка! Повторите попытку!)\n" << '\7';
+		}
+		else
+		{
+			hours_td = 10 * (time_departure[0] - '0') + (time_departure[1] - '0');
+			minutes_td = 10 * (time_departure[3] - '0') + (time_departure[4] - '0');
+			second_td = hours_td * 3600 + minutes_td * 60;
+			break;
+		}
+	}
+
+	while (true)
+	{
+		cout << "Введите время прибытия (HH:MM): ";
+		cin >> time_arrival;
+
+		if ((time_arrival.length() != 5)
+			|| (time_arrival[2] != ':')
+			|| (time_arrival[0] < '0')
+			|| (time_arrival[0] > '2')
+			|| (time_arrival[1] < '0')
+			|| (time_arrival[1] > '3')
+			|| (time_arrival[3] < '0')
+			|| (time_arrival[3] > '5')
+			|| (time_arrival[4] < '0')
+			|| (time_arrival[4] > '9'))
+		{
+			cout << "Error! Try again! (Ошибка! Повторите попытку!)\n" << '\7';
+		}
+		else
+		{
+			hours_ta = 10 * (time_arrival[0] - '0') + (time_arrival[1] - '0');
+			minutes_ta = 10 * (time_arrival[3] - '0') + (time_arrival[4] - '0');
+			second_ta = hours_ta * 3600 + minutes_ta * 60;
+			break;
+		}
+	}
+
+	if (second_td > second_ta)
+	{
+		time_second_result = (second_day - second_td) + second_ta;
+	}
+	else if (second_td < second_ta)
+	{
+		time_second_result = second_ta - second_td;
+	}
+	
+	hours_result = time_second_result / 3600;
+	minutes_result = (time_second_result % 3600) / 60;
+
+	if (hours_result > 23 || minutes_result > 59 || hours_result < 0 || minutes_result < 0)
+	{
+		cout << "Error! Incorrect time format! (Ошибка! Неправильный формат времени!)\n" << '\7';
+	}
+	else
+	{
+		cout << "Поездка составила ";
+		cout << hours_result << " ч. " << minutes_result << " мин.";
+	}
+
+	return 0;
+}
---
+++ A_C++_developer_from_scratch/9.7/exercise_2.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    string str_num;
+    bool seen_dot = false;      // метка точки
+    bool seen_digit = false;    // метка цифры
+    bool valid = true;          // истинная метка
+
+    cout << "Введите число: ";
+    cin >> str_num;
+
+    for (int i = 0; i < str_num.length() && valid; i++)
+    {
+        char c = str_num[i];
+
+        // проверка на является ли символ цифрой
+        bool is_digit = (c >= '0' && c <= '9');
+
+        if (i == 0) // проверки на цифру, точку или минус
+        {
+            if (c == '-') 
+            {
+                // Знак минус - допустимый первый символ (проверка)
+                if (str_num.length() == 1) 
+                {
+                    valid = false; 
+                }
+            }
+            else if (c == '.') 
+            {
+                seen_dot = true;
+            }
+            else if (is_digit) 
+            {
+                seen_digit = true;
+            }
+            else // Недопустимый символ
+            {
+                valid = false; 
+            }
+        }
+        else 
+        {
+            if (c == '.') // Последующие символы: цифра или точка (только одна)
+            {
+                if (seen_dot) // Если вторая точка - ошибка
+                {
+                    valid = false; 
+                }
+                else 
+                {
+                    seen_dot = true;
+                }
+            }
+            else if (is_digit) 
+            {
+                seen_digit = true;
+            }
+            else // Если недопустимый символ
+            {
+                valid = false; 
+            }
+        }
+    }
+
+    if (valid && seen_digit) 
+    {
+        cout << "Yes";
+    }
+    else 
+    {
+        cout << "No";
+    }
+
+    return 0;
+}
