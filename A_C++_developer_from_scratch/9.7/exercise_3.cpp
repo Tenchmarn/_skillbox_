364e8b25a7d32ad83a9c848ddc1afa81e8de17dc
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-14 16:04:56 +0300

    Add files via upload

---
+++ A_C++_developer_from_scratch/9.7/exercise_3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    string str_input;
+    int number_words = 0;
+    bool in_word = false;
+
+    cout << "Введите строку: ";
+    getline(cin, str_input);
+
+    for (int count = 0; count < str_input.length(); count++)
+    {
+        if (str_input[count] != ' ')
+        {
+            if (!in_word)
+            {
+                number_words++;
+                in_word = true;
+            }
+        }
+        else
+        {
+            in_word = false;
+        }
+    }
+
+    cout << "Ответ: " << number_words << '\n';
+
+    return 0;
+}
